851764360713886990485284ad85d7b83b7f0212
Areeb Ahmad Siddiqui <sareebahmad@gmail.com>
2022-11-04 21:47:45 +0530

    Updates

---
+++ precache-manifest.90633c3c35853303524e03a37ca6eb30.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "19213c7d1f1b4d795de2",
+    "url": "/React-Expense-Tracker/static/css/main.192aac77.chunk.css"
+  },
+  {
+    "revision": "19213c7d1f1b4d795de2",
+    "url": "/React-Expense-Tracker/static/js/main.19213c7d.chunk.js"
+  },
+  {
+    "revision": "a1e0fd0ddb88c63ec093",
+    "url": "/React-Expense-Tracker/static/js/1.a1e0fd0d.chunk.js"
+  },
+  {
+    "revision": "aeb3c97a7343c9abdc9e",
+    "url": "/React-Expense-Tracker/static/js/runtime~main.aeb3c97a.js"
+  },
+  {
+    "revision": "428cfac66091c9e2691171c69f3b737e",
+    "url": "/React-Expense-Tracker/index.html"
+  }
+];
